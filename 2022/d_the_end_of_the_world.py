c646a6239782aa09b9f2c1a9ae0f4b64b5e2e6fd
mfrankic <marin.frankic@eglas.hr>
2022-05-20 14:00:29 +0200

    the end of the world

--- 2022/d_the_end_of_the_world.py
+++ 2022/d_the_end_of_the_world.py
@@ -1,34 +1,31 @@
 import math
 
 
-def main(y1, y2, a):
-    # if y2 == 1:
-    #     return -1
-
-    vel = math.sqrt((y1-y2)**2 + 1)
-    _a = -math.pi / 2
-    # _a = math.atan2(y2-y1, 1)
-    a = a * math.pi / 180
-    h = y2
+def main(h, h2, d):
+    angle = 90 - math.degrees(math.atan(h - h2))
+    new_angle = angle
+    height = h2
+    new_height = height
     count = 0
-    # print()
-    while h >= 1:
-        vel_y = vel * math.sin(_a)
-        # print(vel_y, h, _a*180/math.pi)
-        _a += a
-        count += 1
-        if _a >= 0:
-            return count
-        h -= abs(vel_y)
-    return -1
 
-    # count = math.ceil(-_a / a)
+    if angle >= 90:
+      print(0)
+    elif height <= 1 or h <= 1:
+      print(-1)
 
-    # return count if count*(y1-y2) > 0 else -1
+    while new_height >= 1 and height > 1:
+      count += 1
+      height = new_height
+      new_angle += d
+      new_height = height - math.tan(math.radians(new_angle))
+      if new_height < 1 and new_angle < 90:
+        return -1
+      if new_angle >= 90:
+        return count
 
 
 if __name__ == "__main__":
-    y1 = int(input())
-    y2 = int(input())
-    a = int(input())
-    print(main(y1, y2, a))
+    h = int(input())
+    h2 = int(input())
+    d = int(input())
+    print(main(h, h2, d))
